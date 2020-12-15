26b617d4a743248d277ae2b1b9188f5e4d7f6574
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-15 18:20:43 +0500

    created firebase.js

---
+++ src/firbase.js
@@ -0,0 +1,8 @@
+const config ={    
+    apiKey: "AIzaSyD1urU-y4VJd6sJXqsT9BGi6wF5FHXbaV4",
+    authDomain: "firstpwa-67cd6.firebaseapp.com",
+    projectId: "firstpwa-67cd6",
+    storageBucket: "firstpwa-67cd6.appspot.com",
+    messagingSenderId: "987701277666",
+    appId: "1:987701277666:web:cdd7e2090b58caa4b6ffbb"
+}
